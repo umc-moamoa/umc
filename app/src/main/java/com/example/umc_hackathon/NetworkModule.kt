8ba0efe83134a551ad47a077dcb04b8e3bd7b51a
kimehhk <kimehhk@gmail.com>
2022-07-10 18:33:47 +0900

    ADD post model, networkmodule

--- app/src/main/java/com/example/umc_hackathon/NetworkModule.kt
+++ app/src/main/java/com/example/umc_hackathon/NetworkModule.kt
@@ -1,4 +1,13 @@
 package com.example.umc_hackathon
 
-class NetworkModule {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+const val BASE_URL = "https://url" //추후 수정
+
+fun getRetrofit(): Retrofit {
+    val retrofit = Retrofit.Builder().baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create()).build()
+
+    return retrofit
 }
--- app/src/main/java/com/example/umc_hackathon/Post.kt
+++ app/src/main/java/com/example/umc_hackathon/Post.kt
@@ -1,7 +1,16 @@
 package com.example.umc_hackathon
 
 import com.google.gson.annotations.SerializedName
+import java.util.*
 
 data class Post(
-    @SerializedName(value = "title") var title: String
-)
+    @SerializedName(value = "postId") var postId: Long,
+    @SerializedName(value = "title") var title: String,
+    @SerializedName(value = "userId") var userId: Long,
+    @SerializedName(value = "point") var point: Int,
+    @SerializedName(value = "categoryId") var category: Int,
+    //카테고리를 string처리 나중에
+    @SerializedName(value = "content") var content: String,
+    @SerializedName(value = "status") var status: Boolean,
+    @SerializedName(value = "deadline") var deadline: Date
+    )
