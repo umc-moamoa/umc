9915d5cb2718f9e65a0760c8f18bcc01f9eba551
Hajin74 <yoonmij9974@naver.com>
2022-07-15 21:55:44 +0900

    FormCreateActivity 리사이클러뷰 initial

--- app/src/main/java/com/example/umc_hackathon/FormCreateActivity.kt
+++ app/src/main/java/com/example/umc_hackathon/FormCreateActivity.kt
@@ -2,14 +2,28 @@ package com.example.umc_hackathon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.umc_hackathon.databinding.ActivityFormCreateBinding
 import com.example.umc_hackathon.databinding.ActivityFormDetailBinding
 
 class FormCreateActivity : AppCompatActivity() {
+
+    val TAG: String = "<FormCreateActivity>"
+    var modelList = ArrayList<MyQuestion>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityFormCreateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 리스트 생성
+        for (i in 1..10){
+            val myQuestion = MyQuestion(title = "$i.질문을 적겠습니까? ")
+            this.modelList.add(myQuestion)
+        }
+
+        binding.rvFormCreate.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        binding.rvFormCreate.setHasFixedSize(true)
+        binding.rvFormCreate.adapter = MyRecyclerAdapter2(modelList)
     }
 }
