6d373bee456f73541bca673cda16649a2d72eafa
yeji1008 <willy019@naver.com>
2022-07-18 17:54:18 +0900

    layout

---
+++ app/src/main/java/com/example/umc_hackathon/FormDetailActivity.kt
@@ -0,0 +1,11 @@
+package com.example.umc_hackathon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FormDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_form_detail)
+    }
+}
