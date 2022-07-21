7019e9658dcd5edd3803071df29c4262841cb79e
Hajin74 <yoonmij9974@naver.com>
2022-07-21 15:40:55 +0900

    버튼 비클릭시 스타일 xml, 로그인 <-> 회원가입 인텐트

--- app/src/main/java/com/example/umc_hackathon/LoginActivity.kt
+++ app/src/main/java/com/example/umc_hackathon/LoginActivity.kt
@@ -1,11 +1,21 @@
 package com.example.umc_hackathon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.umc_hackathon.databinding.ActivityLoginBinding
+import com.example.umc_hackathon.databinding.ActivityMyPageBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        val binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.loginPageJoinBtn.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
