0c89423fe2d82e56c2556d35e541fe85edaf55e8
Hajin74 <yoonmij9974@naver.com>
2022-07-21 17:12:59 +0900

    마이페이지 수정

--- app/src/main/java/com/example/umc_hackathon/LoginActivity.kt
+++ app/src/main/java/com/example/umc_hackathon/LoginActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.umc_hackathon.databinding.ActivityLoginBinding
-import com.example.umc_hackathon.databinding.ActivityMyPageBinding
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +13,12 @@ class LoginActivity : AppCompatActivity() {
 
         binding.loginPageJoinBtn.setOnClickListener {
             val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        binding.loginPageSubmitBtn.setOnClickListener {
+            val intent = Intent(this, MyPageActivity::class.java)
             startActivity(intent)
             finish()
         }
