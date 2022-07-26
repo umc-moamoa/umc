13bf2b2cde4720ec59cc923dd4bcd0da13ccbbd6
Hajin74 <yoonmij9974@naver.com>
2022-07-26 21:42:41 +0900

    메인 페이지, 참여를 기다리는 설문조사 리사이클러뷰 적용

---
+++ app/src/main/java/com/example/umc_hackathon/WaitingSurveyListRAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.umc_hackathon
+
+import android.content.Intent
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.recyclerview.widget.RecyclerView
+
+class WaitingSurveyListRAdapter(val surveyList: ArrayList<MySurvey>): RecyclerView.Adapter<WaitingSurveyListRAdapter.MyViewHolder>()  {
+
+    val TAG: String = "<WaitingSLRAdapter>"
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WaitingSurveyListRAdapter.MyViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.waiting_survey_board_item, parent, false)
+        return MyViewHolder(view)
+
+//            .apply {
+//            itemView.setOnClickListener {
+//                val currentPosition: Int = adapterPosition
+//                val survey: MySurvey = surveyList.get(currentPosition)
+//                val intent = Intent(itemView?.context, FormDetailActivity::class.java)
+//                intent.putExtra("list_title", survey.title.toString())
+//                ContextCompat.startActivity(itemView.context, intent, null)
+//            }
+//        }
+    }
+
+    override fun getItemCount(): Int {
+        return surveyList.size
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val title = itemView.findViewById<TextView>(R.id.waiting_survey_board_item_title_tv)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        Log.d(TAG, " - onBindViewHolder() called / position: $position")
+        holder.title.text = surveyList.get(position).title
+    }
+
+}
