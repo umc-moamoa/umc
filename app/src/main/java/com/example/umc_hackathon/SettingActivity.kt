5afb8ecc5a6944b0839e529962cc86e35b0b45d5
Hajin74 <yoonmij9974@naver.com>
2022-08-26 02:45:24 +0900

    포인트 내역 스피너 고치기

--- app/src/main/java/com/example/umc_hackathon/SettingActivity.kt
+++ app/src/main/java/com/example/umc_hackathon/SettingActivity.kt
@@ -16,7 +16,7 @@ class SettingActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding = ActivitySettingBinding.inflate(layoutInflater)
+        binding = ActivitySettingBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         binding.settingCancelTv.setOnClickListener {
--- app/src/main/java/com/example/umc_hackathon/post/MyPointView.kt
+++ app/src/main/java/com/example/umc_hackathon/post/MyPointView.kt
@@ -2,8 +2,5 @@ package com.example.umc_hackathon.post
 
 
 interface MyPointView {
-    fun onGetMyRecentPointSuccess(myPointList: MyPointResponse)
-    fun onGetMyFormerPointSuccess(myPointList: MyPointResponse)
-    fun onGetMyTotalPointSuccess(code: Int, result: MyPointList)
     fun onGetMyPointFailure()
 }
